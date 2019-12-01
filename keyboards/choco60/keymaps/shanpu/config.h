82a142195baad302bde882437a4924ee3d547bb3
chez-shanpu <cheztomo513@gmail.com>
2019-12-01 15:08:20 +0900

    Add choco60 shanpu's map

---
+++ keyboards/choco60/keymaps/shanpu/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2019 Naoto Takai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+// 他のキーを押さなければ長押し判定になるタイミングでも単押しにする
+#define RETRO_TAPPING
+
+// 単押し判定のタイミングでも他のキーを一緒に押したら長押し判定にする
+#define PERMISSIVE_HOLD
